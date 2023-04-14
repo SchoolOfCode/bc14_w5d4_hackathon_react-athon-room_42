9252d9c4e12edc0a5ffb53d58bf7cea95c465783
percygr <percyg@gmail.com>
2023-04-14 10:33:56 +0100

    basic setup

---
+++ to-do-list-app/src/components/App/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Input from "../Input";
+import List from "../List";
+
+function App() {
+  return (
+    <div id="app">
+      <Input />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ to-do-list-app/src/components/Input/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Input() {
+  return (
+    <header>
+      <h1>Input goes here</h1>
+    </header>
+  );
+}
+
+export default Input;
