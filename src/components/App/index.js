36157b46245693bca15a25cfc46d39453766e765
percygr <percyg@gmail.com>
2023-04-14 14:16:43 +0100

    broken up down buttons

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -21,6 +21,29 @@ function App() {
     setTodos(todos.filter((item) => item.id !== itemId));
   }
 
+  function upItem(itemId) {
+    console.log(itemId);
+    //[todos[itemId], todos[itemId + 1]] = [todos[itemId + 1], todos[itemId]];
+    // let temp = todos[itemId];
+    // setTodos((todos[itemId] = todos[itemId + 1]));
+    // setTodos((todos[itemId + 1] = temp));
+  }
+
+  function downItem(itemId) {
+    console.log("before", todos);
+
+    const newTodos = [...todos];
+
+    let item1 = newTodos[itemId];
+    let item2 = newTodos[itemId - 1];
+
+    newTodos[itemId] = item2;
+    newTodos[itemId - 1] = item1;
+
+    console.log("after", newTodos);
+    setTodos(newTodos);
+  }
+
   return (
     <div id="app">
       <h1>To Do List</h1>
@@ -36,6 +59,12 @@ function App() {
             text={item.text}
             onClick={() => {
               deleteItem(item.id);
+            }}
+            upClick={() => {
+              upItem(item.id);
+            }}
+            downClick={() => {
+              downItem(item.id);
             }}
           />
         );
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -6,6 +6,9 @@ function List(props) {
   return (
     <div>
       <button onClick={props.onClick}>Delete</button>
+      <button onClick={props.upClick}>Up</button>
+      <button onClick={props.downClick}>Down</button>
+
       {props.text}
     </div>
   );
