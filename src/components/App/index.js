c1dfde2b53f2177bea5f24e5bd40eedad1187c82
Trip25 <t.gurung1995@gmail.com>
2023-04-14 15:08:33 +0100

    working up button now :D

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -23,25 +23,40 @@ function App() {
 
   function upItem(itemId) {
     console.log(itemId);
-    //[todos[itemId], todos[itemId + 1]] = [todos[itemId + 1], todos[itemId]];
-    // let temp = todos[itemId];
-    // setTodos((todos[itemId] = todos[itemId + 1]));
-    // setTodos((todos[itemId + 1] = temp));
+    const newTodos = [...todos];
+    // Finds the index of the item with the id of itemId
+    let todoIndex = todos.findIndex((item) => item.id === itemId);
+    // If the item is already at the top of the list, return
+    if (todoIndex === 0) { return; }
+
+    // Creates a copy of the item at the index of todoIndex and stores it as item1
+    let item1 = { ...newTodos[todoIndex] };
+    // Creates a copy of the item at the index of todoIndex - 1 and stores it as item2
+    let item2 = { ...newTodos[todoIndex - 1] };
+
+     // Swaps the two items in the newTodos array
+      newTodos[todoIndex ] = item2;
+      newTodos[todoIndex - 1] = item1;
+      //Updates the state of the component with the new 'newTodos' array using the 'setTodos' function
+      setTodos(newTodos);
   }
 
   function downItem(itemId) {
-
+//  Creates a copy of todos array using the spread operator and stores it as newTodos
     const newTodos = [...todos];
+    // Finds the index of the item with the id of itemId
     let todoIndex = todos.findIndex((item) => item.id === itemId);
-
+    // If the item is already at the bottom of the list, return
     if (todoIndex === todos.length - 1) { return; }
-
+    // Creates a copy of the item at the index of todoIndex and stores it as item1
     let item1 = { ...newTodos[todoIndex] };
+    // Creates a copy of the item at the index of todoIndex + 1 and stores it as item2
     let item2 = { ...newTodos[todoIndex + 1] };
 
+    // Swaps the two items in the newTodos array
     newTodos[todoIndex ] = item2;
     newTodos[todoIndex + 1] = item1;
-
+    //Updates the state of the component with the new 'newTodos' array using the 'setTodos' function
     setTodos(newTodos);
   }
 
